cc156d1c53b29a62a0a873a41cb5ef8f16375953
Austin Alexander <alexander.648@osu.edu>
2015-12-23 14:05:11 -0500

    flushed out features

--- beeps.py
+++ beeps.py
@@ -7,51 +7,51 @@ import getopt
 BITRATE = 8000
 BITDEPTH = 8
 SIGNED = False
+MAX = (1<<BITDEPTH)-1
+MIN = 0
 optlist, args = getopt.getopt(sys.argv[1:],'sr:d:')
 for opt,arg in optlist:
   if opt == '-d':
-    BITDEPTH = int(arg)
+    BITDEPTH=int(arg)
   if opt == '-s':
     SIGNED = True
   if opt == '-r':
     BITRATE = int(arg)
 
-##############
-BITDEPTH = 8 #
-##############
 
-sine = []
 if SIGNED:
-  for x in range(int(BITRATE)):
-    sine.append(int(((1<<BITDEPTH) -1) * math.sin(float(x)/BITRATE * 2.0 * math.pi)))
-else:
-  for x in range(int(BITRATE)):
-    sine.append(int(((1<<BITDEPTH) -1) * (1+math.sin(float(x)/BITRATE * 2.0 * math.pi))/2.0))
+  MAX=(1<<BITDEPTH-1)-1
+  MIN=(-1<<BITDEPTH-1)
 
+quiet = int((MAX+MIN)/2)
+sine = []
+for x in range(int(BITRATE)):
+  scale = (1+math.sin(float(x)/BITRATE * 2.0 * math.pi))/2.0
+  sine.append(int(MIN+MAX*scale))
+
+
+##################################
+def writebytes(num,bits):
+  while bits >0:
+    sys.stdout.write(chr(num&0xFF))
+    num = num >> 8
+    bits -=8
+
+    
 ##################################
 
 def tone(freq,length):
   for x in range(int(length*BITRATE)):
-    sys.stdout.write(chr(sine[(x*freq)%BITRATE]))
-  
+    writebytes(sine[(x*freq)%BITRATE],BITDEPTH)
 
 def silence(length):
-  if SIGNED:
     for x in range(int(length*BITRATE)):
-      sys.stdout.write('\0')
-
-  else:
-    for x in range(int(length*BITRATE)):
-      sys.stdout.write('\x7F')
+      writebytes(quiet,BITDEPTH)
 
 def noise(length):
-  if SIGNED:
-    for x in range(int(length*BITRATE)):
-      sys.stdout.write(chr(random.randint(0,255)))
+  for x in range(int(length*BITRATE)):
+    writebytes(random.randint(MIN,MAX))
 
-  else:
-    for x in range(int(length*BITRATE)):
-      sys.stdout.write(chr(random.randint(0,255)))
 
 ####################################
 
